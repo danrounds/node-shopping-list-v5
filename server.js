218971843dcfabe56f7f16394ffdab18d0e8edaa
Dan Rounds <dan.rounds@gmail.com>
2016-12-27 21:02:07 -0500

    add PUT method for /recipes

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
   }
   if (req.params.id !== req.body.id) {
     const message = (
-      `Request path id (${req.params.id}) and request body id `
+      `Request path id (${req.params.id}) and request body id ` +
       `(${req.body.id}) must match`);
     console.error(message);
     return res.status(400).send(message);
@@ -104,6 +104,35 @@ app.post('/recipes', jsonParser, (req, res) => {
   }
   const item = Recipes.create(req.body.name, req.body.ingredients);
   res.status(201).json(item);
+});
+
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients', 'id'];
+  for (let i in requiredFields) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}' in request body'`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    // this seems pedantic, but it was in the spec
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating shopping list item \`${req.params.id}'`);
+  const updatedItem = Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).json(updatedItem);
+
+
 });
 
 app.delete('/recipes/:id', (req, res) => {
